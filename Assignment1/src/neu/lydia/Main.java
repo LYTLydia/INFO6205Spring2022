667ed06d41ca8384f0296bcd4cafdb8a800ed7ea
LYTLydia <98440303+LYTLydia@users.noreply.github.com>
2022-01-26 22:14:37 -0800

    Assignment1 Coding

--- Assignment1/src/neu/lydia/Main.java
+++ Assignment1/src/neu/lydia/Main.java
@@ -3,6 +3,32 @@ package neu.lydia;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
+
+    public void swap(int[]nums,int i,int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void sortColors(int[] nums) {
+        int red = 0, blue = nums.length - 1;
+        int i = 0;
+        while (i <= blue) {
+            if(nums[i] == 0){
+                swap(nums,i++,red++);
+            }
+            else if(nums[i]==2){
+                swap(nums,i,blue--);
+            }
+            else{
+                i++;
+            }
+
+        }
     }
 }
+
+
